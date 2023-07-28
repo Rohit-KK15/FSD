e0b87f243bb40077b6778e2e916eb6c74ffd4b21
Rohit-KK15 <105338950+Rohit-KK15@users.noreply.github.com>
2023-07-28 16:21:15 +0530

    imported icons form FontAwesome

--- Portfolio/src/main.js
+++ Portfolio/src/main.js
@@ -4,8 +4,22 @@ import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import '../src/assets/sass/styles.scss'
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+/* add icons to the library */
+library.add(fas, fab)
+
 loadFonts()
 
 createApp(App)
   .use(vuetify)
+  .component('fa', FontAwesomeIcon)
   .mount('#app')
